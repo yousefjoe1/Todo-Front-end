45f4271893b3ab209b7bafe851c20bf4d5c15dec
yousefjoe1 <yousefmahmoud150@gmail.com>
2024-07-03 16:07:18 +0300

    update create todo func: add bearer token

--- Requests/createTodoFunc.ts
+++ Requests/createTodoFunc.ts
@@ -16,17 +16,21 @@ const createTodoFunc = async (data: TodoItem): Promise<any | ErrorResponse>  => 
   
   let checkCookie = cookies().get('todoUsertk')
 
+  console.log(checkCookie?.value);
+  
+
     if(!checkCookie){
       return {"msg":'Your need to login first'}
     }
 
   try {
-    const resp = await axios.post(`http://localhost:5000/api/todos`,data);
+    const resp = await axios.post(`http://localhost:5000/api/todos`,data,
+      {
+      headers: {Authorization:'Bearer ' + checkCookie.value}
+    });
     revalidatePath('/')
     return {data:resp.data,status: true}
   } catch (er:any) {
-    // console.log(er,'err in createtdfunc.ts');
-    
     return {er: `Error`,isError:true}
   }
 };
